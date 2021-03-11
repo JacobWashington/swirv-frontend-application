a9f86e3e1f7e32000877967581d74006ac959afc
J <jchen_729@hotmail.com>
2021-03-10 22:45:06 -0800

    Profile: minor html change.

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -5,6 +5,9 @@ const Profile = (props) => {
     const {  name, email } = props.user
     return (
         <div>
+            <br />
+            <h2>{name}'s Profile</h2>
+            <br />
             <p>name: {name}</p>
             <p>email: {email}</p>
         </div>
