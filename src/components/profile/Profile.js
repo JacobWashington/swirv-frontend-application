d3ee1ac48b4dd1092fc79dc2300abd9d46c369ef
J <jchen_729@hotmail.com>
2021-03-10 22:43:11 -0800

    Login: Added redirect to redirect to profile, and add name and email to be displayed

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import "./Profile.css"
 
-const Profile = () => {
+const Profile = (props) => {
+    const {  name, email } = props.user
     return (
         <div>
-            
+            <p>name: {name}</p>
+            <p>email: {email}</p>
         </div>
     );
 }
