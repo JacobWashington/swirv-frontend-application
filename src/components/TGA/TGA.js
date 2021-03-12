7b903e3c61c28ea654e5db44e05e17b9f09ab483
J <jchen_729@hotmail.com>
2021-03-12 01:45:00 -0800

    TGA: Added offer button that change the selected storyline's authId to TGA

---
+++ src/components/TGA/TGA.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+const TGA = (props) => {
+
+    const [storylines, setStorylines] = useState([])
+    useEffect(() => {
+        const fetchStories = async () => {
+            const response = await axios.get('http://localhost:8000/swirv/theGreatAttractor');
+            const data = response.data;
+ 
+            setStorylines(data);
+        }
+        fetchStories();
+    }, [])
+
+    console.log("CHECKING",storylines)
+
+    const storyline = storylines.map((storyline, index)=> {
+        return (
+            <div className="link">
+                <Link to={{
+                    pathname: "/storyline",
+                    state: storyline
+                }}
+                key={index}
+                >
+                    <h3>{storyline.title}</h3>
+                </Link>
+            </div>
+        )
+    })
+
+    return (
+        <div>
+            <br />
+            <h2>The Great Attractor</h2>
+            <br />
+            <h3>Storylines:</h3>
+            {storyline.length ? storyline : <p>Loading...</p>}
+        </div>
+    );
+}
+
+export default TGA;
