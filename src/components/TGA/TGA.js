dbcaebe39d872d2ff5646c76a3f19f183a52f6ca
jacobwashington <jacobwashington.dev@gmail.com>
2021-03-15 09:09:40 -0700

    at the buzzer

--- src/components/TGA/TGA.js
+++ src/components/TGA/TGA.js
@@ -5,31 +5,20 @@ import StorylinesList from "../storylines/storylinesList/StorylinesList";
 const { REACT_APP_SERVER_URL, TGA_ID } = process.env;
 
 const TGA = (props) => {
-  console.log("TGA CURRENT USER >>>", props.currentUser.id)
   // !!!!!!!!!!! UPDATE BEFORE DEPLOYMENT !!!!!!!!!!! //
   // const [authId, setAuthId] = useState(props.currentUser.id);
-  const [authId, setAuthId] = useState('604db56873b61c2f301ed199');
 
-  useEffect(() => {
-    const fetchTGA = async (req, res) => {
-      // !!!!!!!!!!! UPDATE BEFORE DEPLOYMENT !!!!!!!!!!! //
-      const TGA = await axios.get(
-        `${REACT_APP_SERVER_URL}/users/604db56873b61c2f301ed199`
-      );
-      console.log("!!!!!!!! INSIDE TGA !!!!!!!!!!!", TGA);
-      console.log(TGA.data);
-      setAuthId(TGA.data._id);
-    };
-    fetchTGA();
-  }, []);
-
+  let greatAttractor;
+  if (TGA_ID) {
+    greatAttractor = TGA_ID;
+  } else greatAttractor = "the_great_attractor";
 
   return (
     <div className="container">
       <div className="background"></div>
       <div className="content">
         <p>The Great Attractor</p>
-        <StorylinesList currentUser={authId} />
+        <StorylinesList currentUser={greatAttractor} />
       </div>
     </div>
   );
