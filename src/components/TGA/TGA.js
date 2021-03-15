3ef10c8201248a048ee167fac63025b965ba49ba
J <jchen_729@hotmail.com>
2021-03-14 17:39:34 -0700

    TGA page to show all storylines with authID:the_great_attractor

--- src/components/TGA/TGA.js
+++ src/components/TGA/TGA.js
@@ -5,21 +5,24 @@ import StorylinesList from "../storylines/storylinesList/StorylinesList";
 const { REACT_APP_SERVER_URL, TGA_ID } = process.env;
 
 const TGA = (props) => {
+  console.log("TGA CURRENT USER >>>", props.currentUser.id)
   // !!!!!!!!!!! UPDATE BEFORE DEPLOYMENT !!!!!!!!!!! //
-  const [authId, setAuthId] = useState("");
+  // const [authId, setAuthId] = useState(props.currentUser.id);
+  const [authId, setAuthId] = useState('the_great_attractor');
 
-  useEffect(() => {
-    const fetchTGA = async (req, res) => {
-      // !!!!!!!!!!! UPDATE BEFORE DEPLOYMENT !!!!!!!!!!! //
-      const TGA = await axios.get(
-        `${REACT_APP_SERVER_URL}/users/604db56873b61c2f301ed199`
-      );
-      console.log("!!!!!!!! INSIDE TGA !!!!!!!!!!!", TGA);
-      console.log(TGA.data);
-      setAuthId(TGA.data._id);
-    };
-    fetchTGA();
-  }, []);
+  // useEffect(() => {
+  //   const fetchTGA = async (req, res) => {
+  //     // !!!!!!!!!!! UPDATE BEFORE DEPLOYMENT !!!!!!!!!!! //
+  //     const TGA = await axios.get(
+  //       // `${REACT_APP_SERVER_URL}/users/${authId}`
+  //       `${REACT_APP_SERVER_URL}/storylines/all/the_great_attractor`
+  //     );
+  //     console.log("!!!!!!!! INSIDE TGA !!!!!!!!!!!", TGA);
+  //     console.log(TGA.data);
+  //     setAuthId(TGA.data._id);
+  //   };
+  //   fetchTGA();
+  // }, []);
 
   return (
     <div className="container">
